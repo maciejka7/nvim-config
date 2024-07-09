fcfdd271c0bc98701168760ef7779d8decfe92de
Michal Maciejewski <maciejewskimichal7@gmail.com>
2024-07-09 11:30:09 +0200

    feat: add laravel plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+vim.keymap.set("n", "<C-z>", ":u<CR>", { desc = "Undo" })
