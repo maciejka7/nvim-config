c147b026195b92ae89ad1e8ae907d008277ddea3
Michal Maciejewski <maciejewskimichal7@gmail.com>
2025-01-29 13:00:14 +0100

    feat(theme): add rose-pine variant switcher

--- init.lua
+++ init.lua
@@ -2,3 +2,13 @@
 require("config.lazy")
 
 vim.g.colors_name = "rose-pine"
+
+local variant = { "main", "moon", "dawn" }
+local index = 1
+
+vim.keymap.set("n", "<leader>cv", function()
+  index = index % #variant + 1
+  require("rose-pine").setup({ variant = variant[index] })
+  vim.cmd("colorscheme rose-pine")
+  print("Switched to rose-pine-" .. variant[index])
+end, { desc = "Cycle Rose Pine Variants" })
