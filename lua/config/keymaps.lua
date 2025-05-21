a4149bd82c95ad6d859de80e87530d77a20c2b0f
maciejka7 <maciejewskimichal7@gmail.com>
2025-05-21 13:45:45 +0200

    feat lazyvim config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,7 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+vim.keymap.set("n", "<C-z>", ":u<CR>", { desc = "Undo" })
+-- vim.keymap.set("n", "<C-k>", "vim.lsp.buf.hover()", { desc = "LSP hover information" })
+vim.keymap.set("n", "<leader>fv", "<cmd>Neotree<cr>")
